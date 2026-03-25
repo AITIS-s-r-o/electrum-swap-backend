948a9276e8f4172a7eeb58e39148fadace4e77fe
dejfcz <dejfcz@users.noreply.github.com>
2026-03-25 08:09:04 +0000

    Update daemon.sh
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>

--- daemon.sh
+++ daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./run_electrum stop &&\
-./run_electrum daemon -d &&\
-./run_electrum load_wallet
+./run_electrum stop && \
+    ./run_electrum daemon -d && \
+    ./run_electrum load_wallet
