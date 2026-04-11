4bc9654b232ff56cdf110475c1d3f4bf02995981
dejfcz <dejfcz@users.noreply.github.com>
2026-04-11 19:01:42 +0100

    regtest daemon (#8)

--- daemon.sh
+++ daemon.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-./run_electrum stop 2>/dev/null || true
-./run_electrum daemon -d
-./run_electrum load_wallet
+set -euo pipefail
+
+echo "# Stop electrum if it runs"
+./run_electrum --regtest stop 2>/dev/null || true
+
+echo "# Start electrum in daemon mode"
+./run_electrum --regtest daemon -d
+
+echo "# Load electrum wallet"
+./run_electrum --regtest load_wallet
+echo "✅  Electrum daemon is ready with wallet loaded."
+
+./run_electrum --regtest stop 2>/dev/null || true
+./run_electrum --regtest daemon -d
+./run_electrum --regtest load_wallet
 echo "✅  Electrum daemon is ready with wallet loaded."
