adca79109fd3d693739eff66ffc1654bfaf1fb9c
dejfcz <dejfcz@users.noreply.github.com>
2026-04-11 16:15:20 +0100

    regtest

--- daemon.sh
+++ daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./run_electrum stop 2>/dev/null || true
-./run_electrum daemon -d
-./run_electrum load_wallet
+./run_electrum --regtest stop 2>/dev/null || true
+./run_electrum --regtest daemon -d
+./run_electrum --regtest load_wallet
 echo "✅  Electrum daemon is ready with wallet loaded."
