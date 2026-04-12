ea4c3a7659304d2d69043cf26a42a1082c85212b
dejfcz <dejfcz@users.noreply.github.com>
2026-04-12 12:46:22 +0100

    fix

--- daemon.sh
+++ daemon.sh
@@ -11,8 +11,3 @@ echo "# Start electrum in daemon mode"
 echo "# Load electrum wallet"
 ./run_electrum --regtest load_wallet
 echo "✅  Electrum daemon is ready with wallet loaded."
-
-./run_electrum --regtest stop 2>/dev/null || true
-./run_electrum --regtest daemon -d
-./run_electrum --regtest load_wallet
-echo "✅  Electrum daemon is ready with wallet loaded."
