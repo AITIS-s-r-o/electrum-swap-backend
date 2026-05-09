da85df36517b0330ed729b95ff0adfe3848d3d5c
MartyIX <203266+MartyIX@users.noreply.github.com>
2026-05-09 22:19:39 +0200

    rev

--- electrum/submarine_swaps.py
+++ electrum/submarine_swaps.py
@@ -845,7 +845,7 @@ class SwapManager(Logger):
             server_pubkey=our_pubkey,
             client_pubkey=refund_pubkey,
         )
-        swap = self.add_reverse_swap(
+        swap = self.add_reverse_swap_v1(
             redeem_script=redeem_script,
             locktime=locktime,
             privkey=privkey,
@@ -864,6 +864,52 @@ class SwapManager(Logger):
         privkey: bytes,
         lightning_amount_sat: int,
         onchain_amount_sat: int,
+        preimage: bytes,
+        payment_hash: bytes,
+        prepay_hash: Optional[bytes] = None,
+        claim_to_output: Optional[TxOutput] = None,
+    ) -> SwapData:
+        if payment_hash.hex() in self._swaps:
+            raise Exception("payment_hash already in use")
+        assert sha256(preimage) == payment_hash
+        lockup_address = script_to_p2wsh(redeem_script)
+        if claim_to_output is not None:
+            # the claim_to_output value needs to be lower than the funding utxo value, otherwise
+            # there are no funds left for the fee of the claim tx
+            assert claim_to_output.value < onchain_amount_sat, f"{claim_to_output=} >= {onchain_amount_sat=}"
+            claim_to_output = (claim_to_output.address, claim_to_output.value)
+        swap = SwapData(
+            redeem_script=redeem_script,
+            locktime=locktime,
+            privkey=privkey,
+            preimage=preimage,
+            prepay_hash=prepay_hash,
+            lockup_address=lockup_address,
+            onchain_amount=onchain_amount_sat,
+            claim_to_output=claim_to_output,
+            lightning_amount=lightning_amount_sat,
+            is_reverse=True,
+            is_redeemed=False,
+            funding_txid=None,
+            spending_txid=None,
+        )
+        if prepay_hash:
+            if prepay_hash in self._prepayments:
+                raise Exception("prepay_hash already in use")
+            self._prepayments[prepay_hash] = payment_hash
+        swap._payment_hash = payment_hash
+        self._add_or_reindex_swap(swap, is_new=True)
+        self.add_lnwatcher_callback(swap)
+        return swap
+
+    def add_reverse_swap_v1(
+        self,
+        *,
+        redeem_script: bytes,
+        locktime: int,  # onchain
+        privkey: bytes,
+        lightning_amount_sat: int,
+        onchain_amount_sat: int,
         preimage: Optional[bytes] = None,
         payment_hash: bytes,
         prepay_hash: Optional[bytes] = None,
@@ -871,9 +917,6 @@ class SwapManager(Logger):
     ) -> SwapData:
         if payment_hash.hex() in self._swaps:
             raise Exception("payment_hash already in use")
-
-        if preimage is not None:
-            assert sha256(preimage) == payment_hash
 
         lockup_address = script_to_p2wsh(redeem_script)
         if claim_to_output is not None:
