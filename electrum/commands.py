42a284ee1a280aaae7daca4a2425e46e7faf92d6
dejfcz <dejfcz@users.noreply.github.com>
2026-08-14 13:46:32 +0100

    reintroduce PR 36

--- electrum/commands.py
+++ electrum/commands.py
@@ -2287,8 +2287,19 @@ class Commands(Logger):
                 raise TimeoutError("Transport failed to connect in 15 seconds.")
 
             self.logger.info("wex_reverse_swap; About to get reverse swap data.")
-            claim_fee = sm.get_fee_for_txbatcher()
-            self.logger.info(f"wex_reverse_swap; claim_fee='{claim_fee}'")
+
+            offers = transport.get_recent_offers()
+
+            claim_fee = None
+
+            for offer in offers:
+                if offer.server_pubkey == provider_pk:
+                    self.logger.info(f"wex_reverse_swap; claim_fee='{claim_fee}', offer='{offer}'")
+                    claim_fee = offer.pairs.mining_fee
+                    break
+
+            if claim_fee is None:
+                raise TimeoutError(f"Could not find offer for the specified provider '{provider_pk}'.")
 
             onchain_amount_sat = onchain_amount + claim_fee
             self.logger.info(f"wex_reverse_swap; onchain_amount_sat='{onchain_amount_sat}'")
